9094ccde26e14b7babba73974ba75b4d7bd9927d
AnnaM317 <anya317@gmail.com>
2021-12-12 23:09:40 +0200

    changes

--- api/order/order.service.js
+++ api/order/order.service.js
@@ -111,6 +111,7 @@ async function update(order) {
         // return order;
         const collection = await dbService.getCollection('order');
         await collection.updateOne({ _id: orderToSave._id }, { $set: orderToSave });
+
         return order;
     } catch (err) {
         logger.error(`cannot update order ${order._id}`, err);

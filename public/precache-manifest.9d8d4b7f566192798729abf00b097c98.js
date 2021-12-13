d58ff224a95ec9d7f1d70a36a685230d7010b4b7
daniieldvir <91826695+daniieldvir@users.noreply.github.com>
2021-12-13 11:47:22 +0200

    heroku

---
+++ public/precache-manifest.9d8d4b7f566192798729abf00b097c98.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310d28de5fa3952d2c91",
+    "url": "/css/app.7d7a1bc4.css"
+  },
+  {
+    "revision": "0d5c972486395c58314f",
+    "url": "/css/chunk-vendors.fd510710.css"
+  },
+  {
+    "revision": "d8d746a9321767cc1f2c33e0c29bc9bd",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "c94c29feb80b02db176028d16fc634d2",
+    "url": "/fonts/AirbnbCereal-Bold.c94c29fe.ttf"
+  },
+  {
+    "revision": "1f9c7d320137974038c2f1a7300a1577",
+    "url": "/fonts/AirbnbCereal-Light.1f9c7d32.ttf"
+  },
+  {
+    "revision": "d9fb3054e00c65841121da81c48e4fb1",
+    "url": "/fonts/AirbnbCereal-Medium.d9fb3054.ttf"
+  },
+  {
+    "revision": "cb17fa4a508de3daf0d9ea64a0ab724e",
+    "url": "/fonts/Manrope-Bold.cb17fa4a.ttf"
+  },
+  {
+    "revision": "a115fc6209a11c4e1d4344557ae25433",
+    "url": "/fonts/Manrope-Light.a115fc62.ttf"
+  },
+  {
+    "revision": "29b7f4fc75797a82d890da507b02ae92",
+    "url": "/fonts/Manrope-VariableFont_wght.29b7f4fc.ttf"
+  },
+  {
+    "revision": "a748accdd957392acccae07b4dbb29a2",
+    "url": "/img/grid.a748accd.svg"
+  },
+  {
+    "revision": "4f0548efc0c1141174b4f38d8edc6284",
+    "url": "/index.html"
+  },
+  {
+    "revision": "310d28de5fa3952d2c91",
+    "url": "/js/app.cdde4562.js"
+  },
+  {
+    "revision": "0d5c972486395c58314f",
+    "url": "/js/chunk-vendors.885efb96.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
